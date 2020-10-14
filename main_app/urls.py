1fc98d1afeb53ecbb7f163869510f67ffedc6a49
pratik <engrpatel21@gmail.com>
2020-10-14 10:18:36 -0400

    final push

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('add_wish/', views.AddWish.as_view(), name='add_wish'),
+    path('delete_wish/<int:wishlist_id>', views.delete_wish, name='delete_wish')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, DeleteView
+from .models import WishList
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    wishs = WishList.objects.all()
+    return render(request, 'home.html', {'wishs':wishs})
+
+class AddWish(CreateView):
+    model = WishList
+    fields = '__all__'
+    success_url = '/'
+    
+def delete_wish(request, wishlist_id):
+    wish = WishList.objects.get(id=wishlist_id).delete()
+    return redirect('home')
